93e7bd95ac6b65af4535a6b01c837efab445dab9
C0D1NGD0J0 <web-dev@United4Life.local>
2016-12-01 10:29:37 +0100

    store players name in variables / array to hold move options

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,13 @@
+# A simple rock paper scissor game.
+# Basic requirements (V1)
+	# an array to store moves
+	# store players name in a variable
+
+moves = ['rock', 'paper', 'scissors']
+
+print "Player 1 enter name > "
+p1 = gets.chomp.downcase
+
+print "Player 2 enter name > "
+p2 = gets.chomp.downcase
+

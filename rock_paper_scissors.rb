5b5ec0375b6de2c05b6847bc07bd727246e017d4
C0D1NGD0J0 <web-dev@United4Life.local>
2016-12-01 10:37:33 +0100

    player1 able input move & AI able to make a selection from moves options

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,13 +1,22 @@
 # A simple rock paper scissor game.
 # Basic requirements (V1)
 	# an array to store moves
-	# store players name in a variable
+	# store player name in a variable
+
+# Requirements (V2)
+	# user should be able to input move
+	# computer should be able to randomly make the selection based on 3 options
+
 
 moves = ['rock', 'paper', 'scissors']
 
 print "Player 1 enter name > "
 p1 = gets.chomp.downcase
 
-print "Player 2 enter name > "
-p2 = gets.chomp.downcase
+# ====================
 
+print "#{p1} make a move > "
+p1_move = gets.chomp.downcase
+
+print "AI make your move > "
+puts ai = moves.sample

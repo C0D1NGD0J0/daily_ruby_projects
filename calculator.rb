4422b4bb5a504190a404e58bd7e4b95a68148907
C0D1NGD0J0 <web-dev@United4Life.local>
2016-12-05 11:53:51 +0100

    basic calculator v1 completed

--- calculator.rb
+++ calculator.rb
@@ -6,5 +6,29 @@
 		# Divide
 	# REPL
 
+def calculator
+	loop do
+		puts "Enter" 
+		print '> '
+		input = gets.chomp.split(' ')
+		command = input[0].downcase
+		arg1 = input[1].to_f
+		arg2 = input[2].to_f
+		case command
+			when 'quit'
+				break
+			when 'add' || 'Add'
+				puts arg1 + arg2
+			when 'substract' || 'minus'
+				puts arg1 - arg2
+			when 'divide'
+				puts arg1 / arg2
+			when 'multiply'
+				puts arg1 * arg2
+			else
+				puts 'not a valid operator'
+		end
+	end
+end
 
-
+calculator

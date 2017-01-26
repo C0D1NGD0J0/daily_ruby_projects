0badf2ffae2dfefb18ce76d1a16da86d45f766a1
C0D1NGD0J0 <web-dev@United4Life.local>
2017-01-26 13:22:27 +0100

    array rotate method created v1

---
+++ rotate.rb
@@ -0,0 +1,11 @@
+# How to Rotate Elements in an array and change the index value for each element.
+
+# Case: Build a method that takes two arguments, array of values and number of time an element should move.
+# e.g.
+	# array_rotation([1,2,3], 2)
+	# step 1 => [1, 3, 2]
+	# step 2 => [3, 1, 2]
+
+def array_rotation
+
+end

004bb5c010b503fd1302ed1054b2eb539f41d70d
C0D1NGD0J0 <web-dev@United4Life.local>
2017-02-14 13:44:02 +0100

    created the game board

--- tic-tac-toe/board.rb
+++ tic-tac-toe/board.rb
@@ -1,15 +1,21 @@
 class Board
+	WIDTH = 3
+	HEIGHT = WIDTH
 	attr_accessor :grid
 
 	def initialize
-		@grid = [
-			[[], [], []],
-			[[], [], []],
-			[[], [], []]
-		]
+		@grid = Array.new(HEIGHT) { Array.new(WIDTH, :" ") }
 	end
 
 	def print_grid
-		
+		@grid.each do |row|
+			puts
+			row.each do |cell|
+				print "[#{cell}]"
+			end
+		end
+		puts
 	end
 end
+
+Board.new.print_grid
